87704de050dd47f5ff19bdbf47a055fd3472420a
Tomas Elizondo <tomelizif@gmail.com>
2021-08-24 16:34:49 -0300

    Closures y Scope

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,30 @@
-      // class
-      function MediaPlayer(config) {
-        this.media = config.med;
-      }
+// class
+function MediaPlayer(config) {
+  this.media = config.med;
+}
 
-      // object from class
-      const video1 = document.querySelector('#firstVideo');
-      const player = new MediaPlayer({ med: video1 });
+// object from class
+const video1 = document.querySelector('#firstVideo');
+const player = new MediaPlayer({ med: video1 });
 
-      // class methods
-      MediaPlayer.prototype.play = function() {
-        this.media.play();
-      };
+// class methods
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
 
-      MediaPlayer.prototype.pause = function() {
-        this.media.pause();
-      };
-      
-      MediaPlayer.prototype.togglePlay = function() {
-        if (this.media.paused) {
-          this.play();
-        } else {
-          this.pause();
-        }
-      };
-      
-      // button interactivity
-      const button = document.querySelector('button');
-      button.onclick = () => player.togglePlay();
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+// button interactivity
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
+
